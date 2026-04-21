2a72675a1d868bc0e1ae6c274a96932f26c249ef
Devang Parmar <neosoft@Devang-Neosofts-MacBook-Pro.local>
2026-04-21 16:28:37 +0530

    Add initial iOS SwiftUI project

---
+++ ios-app/MediaPlayerApp/MediaPlayerApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MediaPlayerApp
+//
+//  Created by Devang Parmar on 21/04/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ios-app/MediaPlayerApp/MediaPlayerApp/MediaPlayerAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MediaPlayerAppApp.swift
+//  MediaPlayerApp
+//
+//  Created by Devang Parmar on 21/04/26.
+//
+
+import SwiftUI
+
+@main
+struct MediaPlayerAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
